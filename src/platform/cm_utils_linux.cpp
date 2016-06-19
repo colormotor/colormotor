9b3784602dfae89d14462f016edd11779dab4c23
Daniel Berio <drand48@gmail.com>
2016-06-19 14:56:42 +0100

    file watcher

--- src/platform/cm_utils_linux.cpp
+++ src/platform/cm_utils_linux.cpp
@@ -197,7 +197,147 @@ std::string  getExecutablePath()
 	*path_end = '\0';
 	/* The length of the path is the number of characters up through the
 	 last slash.  */
-	return buffer;
+	return std::string(buffer); 
 }
+
+///////////////
+// Gross filewatcher
+
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+
+FileWatcher::FileWatcher( const std::string & path, int msecs  )
+:
+fileFlags(0),
+msecs(msecs)
+{
+  //http://man7.org/tlpi/code/online/diff/inotify/demo_inotify.c.html
+     
+     struct inotify_event *event;
+ 
+     fd = inotify_init();                 /* Create inotify instance */
+     if (fd == -1)
+         assert(0);
+
+  numFiles =  1;
+  
+  int wd = inotify_add_watch(fd, path.c_str(), IN_MODIFY ); //IN_ALL_EVENTS);
+    Wd.push_back(wd);
+
+    paths.push_back(path);
+
+    running = true;
+    thread = std::thread(&FileWatcher::run, this);
+}
+
+FileWatcher::~FileWatcher()
+{
+    stop();
+    for( int i = 0; i < Wd.size(); i++ )
+       inotify_rm_watch( fd, Wd[i] );
+   close(fd);
+     printf("Closed file watcher\n");
+}
+
+bool FileWatcher::hasFileChanged()
+{
+  bool res = fileFlags != 0;
+  fileFlags = 0;
+  return res;
+}
+
+static void             /* Display information from inotify_event structure */
+ displayInotifyEvent(FileWatcher * watcher, struct inotify_event *i)
+ {
+    /*
+     printf("    wd =%2d; ", i->wd);
+     if (i->cookie > 0)
+         printf("cookie =%4d; ", i->cookie);
+    
+    
+     printf("mask = ");
+     if (i->mask & IN_ACCESS)        printf("IN_ACCESS ");
+     if (i->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
+     if (i->mask & IN_CLOSE_NOWRITE) printf("IN_CLOSE_NOWRITE ");
+     if (i->mask & IN_CLOSE_WRITE)   printf("IN_CLOSE_WRITE ");
+     if (i->mask & IN_CREATE)        printf("IN_CREATE ");
+     if (i->mask & IN_DELETE)        printf("IN_DELETE ");
+     if (i->mask & IN_DELETE_SELF)   printf("IN_DELETE_SELF ");
+     if (i->mask & IN_IGNORED)       printf("IN_IGNORED ");
+     if (i->mask & IN_ISDIR)         printf("IN_ISDIR ");
+     if (i->mask & IN_MODIFY)        printf("IN_MODIFY ");
+     if (i->mask & IN_MOVE_SELF)     printf("IN_MOVE_SELF ");
+     if (i->mask & IN_MOVED_FROM)    printf("IN_MOVED_FROM ");
+     if (i->mask & IN_MOVED_TO)      printf("IN_MOVED_TO ");
+     if (i->mask & IN_OPEN)          printf("IN_OPEN ");
+     if (i->mask & IN_Q_OVERFLOW)    printf("IN_Q_OVERFLOW ");
+     if (i->mask & IN_UNMOUNT)       printf("IN_UNMOUNT ");
+     printf("\n");
+    
+*/
+
+    if( i->len < 1 )
+        return;
+
+    std::string name = i->name;
+    if( getFileExt(name) == "py" && getFilenameFromPath(name) != "tmp.py" )
+    {
+        printf("Modified file: %s\n", i->name);
+        watcher->fileFlags = 1;
+    }
+
+ }
+ 
+void FileWatcher::stop()
+{
+    if(running)
+    {
+        mutex.lock();
+        printf("Joining thread\n");
+        running = false;
+        thread.join();
+        printf("Stopped file watching thread\n");
+        mutex.unlock();
+    }
+}
+
+void FileWatcher::run()
+{
+  char *p;
+  struct inotify_event *event;
+  char buf[EVENT_BUF_LEN] __attribute__ ((aligned(8)));
+    printf("Starting file watcher\n");
+
+  while( running )
+  {
+        //mutex.lock();
+        struct pollfd pfd = { fd, POLLIN, 0 };
+        int ret = poll(&pfd, 1, 50);  // timeout of 50ms
+        if (ret < 0) {
+            fprintf(stderr, "poll failed: %s\n", strerror(errno));
+        } else if (ret == 0) {
+            continue;
+        }
+
+    int numRead = read(fd, buf, EVENT_BUF_LEN);
+        
+        if( numRead == 0 )
+    {
+      continue;
+    }
+
+    for (p = buf; p < buf + numRead; ) {
+             event = (struct inotify_event *) p;
+             displayInotifyEvent(this, event);
+         //fileFlags = 1;
+             p += EVENT_SIZE + event->len;
+         }
+         //mutex.unlock();
+         std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    //Thread::sleep(msecs);
+  }
+}
+
 
 }
--- src/platform/cm_utils_osx.mm
+++ src/platform/cm_utils_osx.mm
@@ -5,6 +5,10 @@
 #import <AppKit/AppKit.h>
 #import <Foundation/NSData.h>
 
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+#include <inttypes.h>
 
 namespace cm
 {
@@ -259,6 +263,133 @@ std::string  getExecutablePath()
 	const char* cstring = [bundlePath cStringUsingEncoding:NSASCIIStringEncoding];
 	return std::string(cstring);
 }
+
+// Gross filewatcher
+
+/* A simple routine to return a string for a set of flags. */
+static char *flagstring(int flags)
+{
+    static char ret[512];
+    char *ors = "";
+	
+    ret[0]='\0'; // clear the string.
+    if (flags & NOTE_DELETE) {strcat(ret,ors);strcat(ret,"NOTE_DELETE");ors="|";}
+    if (flags & NOTE_WRITE) {strcat(ret,ors);strcat(ret,"NOTE_WRITE");ors="|";}
+    if (flags & NOTE_EXTEND) {strcat(ret,ors);strcat(ret,"NOTE_EXTEND");ors="|";}
+    if (flags & NOTE_ATTRIB) {strcat(ret,ors);strcat(ret,"NOTE_ATTRIB");ors="|";}
+    if (flags & NOTE_LINK) {strcat(ret,ors);strcat(ret,"NOTE_LINK");ors="|";}
+    if (flags & NOTE_RENAME) {strcat(ret,ors);strcat(ret,"NOTE_RENAME");ors="|";}
+    if (flags & NOTE_REVOKE) {strcat(ret,ors);strcat(ret,"NOTE_REVOKE");ors="|";}
+	
+    return ret;
+}
+
+enum
+{
+	NUM_EVENT_SLOTS = 1,
+	NUM_EVENT_FDS  = 1
+};
+
+FileWatcher::FileWatcher( const std::string & path, int msecs  )
+:
+fileFlags(0),
+msecs(msecs)
+{
+    /* Open a kernel queue. */
+    if ((kq = kqueue()) < 0) {
+        fprintf(stderr, "Could not open kernel queue.  Error was %s.\n", strerror(errno));
+		return;
+    }
+	
+    /*
+	 Open a file descriptor for the file/directory that you
+	 want to monitor.
+     */
+    event_fd = open(path.c_str(), O_EVTONLY);
+    if (event_fd <=0) {
+        fprintf(stderr, "The file %s could not be opened for monitoring.  Error was %s.\n", path.c_str(), strerror(errno));
+        return;
+    }
+	
+	/*
+	 The address in user_data will be copied into a field in the
+	 event.  If you are monitoring multiple files, you could,
+	 for example, pass in different data structure for each file.
+	 For this example, the path string is used.
+     */
+    user_data = (void*)path.c_str();
+	
+	/* Set the timeout to wake us every half second. */
+    timeout.tv_sec = 0;        // 0 seconds
+    timeout.tv_nsec = msecs * 1000000;//1000000;//500000000;    // 500 milliseconds
+	
+    /* Set up a list of events to monitor. */
+    vnode_events = NOTE_DELETE |  NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK | NOTE_RENAME | NOTE_REVOKE;
+    EV_SET( &events_to_monitor[0], event_fd, EVFILT_VNODE, EV_ADD | EV_CLEAR, vnode_events, 0, user_data);
+	
+	numFiles =  1;
+	
+	running = true;
+    thread = std::thread(&FileWatcher::run, this);
+}
+
+FileWatcher::~FileWatcher()
+{
+	//stopThread();
+	stop();
+	::close(event_fd);
+}
+
+bool FileWatcher::hasFileChanged()
+{
+	bool res = fileFlags != 0;
+	fileFlags = 0;
+	return res;
+}
+
+void FileWatcher::stop()
+{
+    if(running)
+    {
+        mutex.lock();
+        printf("Joining thread\n");
+        running = false;
+        thread.join();
+        printf("Stopped file watching thread\n");
+        mutex.unlock();
+    }
+}
+
+
+void FileWatcher::run()
+{
+	while( running )
+	{
+		int event_count = kevent(kq, events_to_monitor, NUM_EVENT_SLOTS, event_data, numFiles, &timeout);
+		if ((event_count < 0) || (event_data[0].flags == EV_ERROR)) {
+			/* An error occurred. */
+			fprintf(stderr, "An error occurred (event count %d).  The error was %s.\n", event_count, strerror(errno));
+		}
+		
+		if (event_count) 
+		{
+            /*
+			printf("Event  occurred.  Filter %d, flags %d, filter flags %s\n",
+				   event_data[0].filter,
+				   event_data[0].flags,
+				   flagstring(event_data[0].fflags));
+			*/
+			fileFlags = event_data[0].fflags;
+		}
+		else
+		{
+			fileFlags = 0;
+		}
+			
+		std::this_thread::sleep_for(std::chrono::milliseconds(msecs));
+	}
+}
+
 
 
 }
