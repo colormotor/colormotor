24d86a9bab629d689fdce883bb1d797abc74ca5d
Daniel Berio <drand48@gmail.com>
2016-10-02 20:26:09 +0100

    render target and ctr contains

--- src/cm_gfx.cpp
+++ src/cm_gfx.cpp
@@ -34,6 +34,7 @@ namespace gfx
 {
 
 
+
 bool init()
 {
 	#ifdef CM_LINUX
@@ -797,7 +798,7 @@ void applyMatrix( const arma::mat& m_ )
 #ifdef GFX_TO_EPS
     if( renderingToEps )
     {
-        eps.setMatrix(m_);
+        eps.applyMatrix(m_);
     }
 #endif
 }
@@ -1278,6 +1279,12 @@ void fill( const Shape& shape, int winding )
         eps.fillShape(shape, currentColor);
     }
 #endif
+}
+
+/// Draw a texture
+void textureQuad( Texture& tex, float x, float y, float w, float h )
+{
+
 }
 
 /// Draw an image
@@ -2153,6 +2160,20 @@ double Contour::length( int a, int b ) const
 	return l;
 }
 
+bool Contour::contains( const V2& p ) const
+{
+ 	int n = size();
+ 	int i, j;
+ 	int c = 0;
+	for( int i = 0, j = n-1; i < n; j = i++) 
+	{
+		if ( ((points.col(i)[1]>p[1]) != (points.col(j)[1]>p[1])) &&
+			(p.x < (points.col(j)[0] - points.col(i)[0]) * (p[1] - points.col(i)[1]) / (points.col(j)[1] - points.col(i)[1]) + points.col(i)[0]) )
+			c = !c;
+	}
+	
+	return (bool)c;
+}
 
 Box Contour::boundingBox() const
 {
@@ -2201,6 +2222,18 @@ Contour Contour::transformed( const arma::mat& m ) const
 
 ////////////////////////////////////////////////////////////////
 // Shape impl
+
+bool Shape::contains( const V2&p ) const
+{
+    int n = 0;
+    for( int i = 0; i < contours.size(); i++ )
+    {
+        if( contours[i].contains( p ) )
+            n++;
+    }
+    
+    return (n%2) == 1;
+}
 
 
 Box Shape::boundingBox() const
@@ -3634,6 +3667,342 @@ bool	Texture::readPixels( void * data, int width, int height, FORMAT fmt )
     return true;
 }
 
+static RenderTarget * currentRT = 0;
+
+static void releaseFbo( GLuint id )
+{
+    
+    if(gfx::isGLReleaseManual())
+        glDeleteFramebuffersEXT(1,&id);
+    else
+        gfx::releaseGLObject(GLObj(id,GLObj::FBO));
+}
+
+static void releaseRb( GLuint id )
+{
+    
+    if(gfx::isGLReleaseManual())
+        glDeleteRenderbuffersEXT(1,&id);
+    else
+        gfx::releaseGLObject(GLObj(id,GLObj::RENDERBUFFER));
+}
+
+////////////////////////////////////////////////////////////
+
+
+RenderTarget::RenderTarget()
+{
+    zero();
+}
+
+void RenderTarget::zero()
+{
+    numColorAttachments = 0;
+    hasDepthStencil = false;
+    numSamples = 0;
+    
+    depthStencilId = 0;
+    fbo = 0;
+    fboTextures = 0;
+    
+    for( int i = 0; i < MAX_COLOR_ATTACHMENTS; i++ )
+    {
+        dirty[i] = false;
+        textureAttachments[i] = 0;
+        colorBuffers[i] = 0;
+    }
+    
+    savedFramebuffer = 0;
+}
+
+RenderTarget::RenderTarget( int w, int h,
+                           int pixelFormat,
+                           bool depthStencil,
+                           int depthFormat,
+                           int numSamples )
+{
+    zero();
+    create2d(w,h,pixelFormat,depthStencil,depthFormat,numSamples);
+}
+
+
+
+////////////////////////////////////////////////////////////
+
+
+
+RenderTarget::~RenderTarget()
+{
+    release();
+}
+
+
+
+////////////////////////////////////////////////////////////
+
+
+void	RenderTarget::release()
+{
+    ;
+    
+    for( int i = 0; i < numColorAttachments; i++ )
+    {
+        if(colorBuffers[i])
+            releaseRb(colorBuffers[i]);
+        colorBuffers[i] = 0;
+        if(textureAttachments[i])
+            delete textureAttachments[i];
+        textureAttachments[i] = 0;
+    }
+    
+    if(fbo == fboTextures)
+    {
+        releaseFbo(fbo);
+    }
+    else
+    {
+        releaseFbo(fbo);
+        releaseFbo(fboTextures);
+    }
+    
+    if(depthStencilId)
+        releaseRb(depthStencilId);
+    depthStencilId = 0;
+    
+    savedFramebuffer = 0;
+   
+    zero();
+}
+
+
+////////////////////////////////////////////////////////////
+bool RenderTarget::createMrt( int w, int h,
+                             std::vector<int> formats,
+                             bool hasDepthStencil,
+                             int depthStencilFormat,
+                             int numSamples )
+{
+    release();
+ 
+ 
+     // check if non power of 2 depth buffers are supported?
+     if( gfx::areNonPowerOfTwoTexturesSupported() )
+     {
+         width = w;
+         height = h;
+     }
+     else
+     {
+         width = NPOT(w);
+         height = NPOT(h);
+     }
+
+    this->numSamples = numSamples;
+    this->hasDepthStencil = hasDepthStencil;
+    this->depthStencilFormat = depthStencilFormat;
+    this->formats = formats;
+ 
+ 
+    // create main fbo
+    // this is the main one we bind for drawing into
+    // all the renderbuffers are attached to this (whether MSAA is enabled or not)
+    glGenFramebuffersEXT(1, &fbo);
+    bind(false);
+    
+ 
+    //currently depth only works if stencil is enabled.
+    // http://forum.openframeworks.cc/index.php/topic,6837.0.html
+ 
+    GLenum glDepthAttachment = GL_DEPTH_ATTACHMENT_EXT;
+    if( depthStencilFormat == DepthStencil::D24_S8)
+        glDepthAttachment = GL_DEPTH_STENCIL_ATTACHMENT;
+        
+    static GLint glFmts[DepthStencil::NUM_FORMATS] =
+    {
+        GL_DEPTH_COMPONENT16,
+        GL_DEPTH_COMPONENT24,
+        GL_DEPTH24_STENCIL8_EXT,
+        GL_DEPTH_COMPONENT32
+    };
+
+    GLint glDepthStencilFormat = glFmts[depthStencilFormat];
+        
+    //- USE REGULAR RENDER BUFFER
+    if(hasDepthStencil)
+    {
+        depthStencilId = createAndAttachRenderbuffer(glDepthStencilFormat, glDepthAttachment);
+    }
+ 
+    // if we want MSAA, create a new fbo for textures
+	if(numSamples){
+		glGenFramebuffersEXT(1, &fboTextures);
+	}else{
+		fboTextures = fbo;
+	}
+ 
+    numColorAttachments = formats.size();
+ 
+    // now create all textures and color buffers
+    if(formats.size())
+    {
+        for(int i=0; i<formats.size(); i++)
+            createAndAttachTexture(formats[i], i);
+    }
+ 
+    // if textures are attached to a different fbo (e.g. if using MSAA) check it's status
+    if(fbo != fboTextures)
+    {
+        glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboTextures);
+    }
+    
+    // check everything is ok with this fbo
+    valid = gfx::checkFramebufferStatus();
+    
+    // unbind it
+    unbind(false);
+ 
+    return valid;
+}
+
+
+
+bool	RenderTarget::resize( int w, int h )
+{
+    if( !valid )
+    {
+        debugPrint( "Render target not initialized, can't resize\n");
+        return false;
+    }
+    
+    return createMrt(w,h,formats,hasDepthStencil,depthStencilFormat,numSamples);
+}
+
+
+
+void RenderTarget::bind(bool viewPort)
+{
+    
+    
+    // set viewport with first color buffer size ( we assume that all buffers have same size )
+    //NOTE: shouldn't the FBO be bound before calling setViewport ?
+    if(viewPort)
+    {
+    	gfx::pushViewport();
+        gfx::setViewport(0,0,textureAttachments[0]->getWidth(),textureAttachments[0]->getHeight());
+    }
+	glGetIntegerv(GL_FRAMEBUFFER_BINDING_EXT, &savedFramebuffer);
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+}
+
+
+void RenderTarget::unbind(bool viewPort)
+{
+    
+    
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, savedFramebuffer);
+    for( int i = 0; i < numColorAttachments; i++ )
+        dirty[i] = true;
+
+    if(viewPort)
+    {
+        gfx::popViewport();
+    }
+}
+
+Texture * RenderTarget::getTexture( int ind )
+{
+    updateTexture(ind);
+    return textureAttachments[ind];
+}
+
+void RenderTarget::updateTexture(int attachmentPoint)
+{
+    if(!valid)
+        return;
+    
+    if(fbo != fboTextures && dirty[attachmentPoint])
+    {
+        glGetIntegerv( GL_FRAMEBUFFER_BINDING_EXT, &savedFramebuffer );
+        
+        glPushAttrib(GL_COLOR_BUFFER_BIT);
+        
+        // save current readbuffer
+        GLint readBuffer;
+        glGetIntegerv(GL_READ_BUFFER, &readBuffer);
+        
+        glBindFramebufferEXT(GL_READ_FRAMEBUFFER_EXT, fbo);
+        glBindFramebufferEXT(GL_DRAW_FRAMEBUFFER_EXT, fboTextures);
+        glDrawBuffer(GL_COLOR_ATTACHMENT0_EXT + attachmentPoint);
+        glReadBuffer(GL_COLOR_ATTACHMENT0_EXT + attachmentPoint);
+        glBlitFramebufferEXT(0, 0, width, height, 0, 0, width, height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+        
+        glBindFramebufferEXT(GL_READ_FRAMEBUFFER_EXT, savedFramebuffer);
+        glBindFramebufferEXT(GL_DRAW_FRAMEBUFFER_EXT, savedFramebuffer);
+        glBindFramebufferEXT( GL_FRAMEBUFFER_EXT, savedFramebuffer );
+        
+        // restore readbuffer
+        glReadBuffer(readBuffer);
+        
+        glPopAttrib();
+        
+        dirty[attachmentPoint] = false;
+    }
+}
+
+
+
+GLuint RenderTarget::createAndAttachRenderbuffer(GLenum internalFormat, GLenum attachmentPoint)
+{
+    
+    
+    GLuint buffer;
+    glGenRenderbuffersEXT(1, &buffer);
+    glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, buffer);
+    if(numSamples==0)
+        glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, internalFormat, width, height);
+    else
+        glRenderbufferStorageMultisampleEXT(GL_RENDERBUFFER_EXT, numSamples, internalFormat, width, height);
+    
+    glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, attachmentPoint, GL_RENDERBUFFER_EXT, buffer);
+    return buffer;
+}
+
+
+void RenderTarget::createAndAttachTexture(int format, int attachmentPoint)
+{
+    
+    
+    // bind fbo for textures (if using MSAA this is the newly created fbo, otherwise its the same fbo as before)
+    GLint temp;
+    glGetIntegerv(GL_FRAMEBUFFER_BINDING_EXT, &temp);
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboTextures);
+    
+    Texture * tex = new Texture();
+    tex->create(width,height,format,1);
+    
+    glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT + attachmentPoint, GL_TEXTURE_2D, tex->info.glId,0);
+    textureAttachments[attachmentPoint] = tex;
+    
+    // if MSAA, bind main fbo and attach renderbuffer
+    if(numSamples) {
+        glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo);
+        
+        GLuint colorBuffer = createAndAttachRenderbuffer(tex->info.glFormat, GL_COLOR_ATTACHMENT0_EXT + attachmentPoint);
+        colorBuffers[attachmentPoint] = colorBuffer;
+    }
+    
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, temp);
+}
+
+
+//////////////////////////////////////////////////////////////////
+
+bool RenderTarget::isValid() const
+{
+    return valid;
+}
+
+
 
 ////////////////////////////////////////////////////////////////
 // Image impl
@@ -4043,10 +4412,7 @@ Shape Image::findContours( bool approx, float minArea, float maxArea )
     }
     
     return shape;
-}
-
-
-	
+}	
 
 V3 ArcBall::projectToSphere( const V2 & p, const V2 & center, float radius, bool constrained, const V3& axis )
 {
--- src/cm_gfx.h
+++ src/cm_gfx.h
@@ -100,6 +100,7 @@ public:
 
 
     arma::vec ltrb() const { return arma::vec({l(), t(), r(), b()}); }
+    void ltrb( double l, double t, double r, double b ) { minmax(0,0)=l; minmax(1,0)=t; minmax(0,1)=r; minmax(1,1)=b; }
 
     void l( double v ) { minmax(0,0)=v; }
     void t( double v ) { minmax(1,0)=v; }
@@ -215,6 +216,8 @@ Contour()
 
     arma::vec centroid() const { return mean(points,1); }
 
+    bool contains( const V2& p ) const;
+
     /// Bounding box of the contour	
     Box boundingBox() const;
 
@@ -283,6 +286,8 @@ struct Shape
     arma::vec centroid() const;
     Box boundingBox() const;
 
+    bool contains( const V2& p ) const;
+   
     void transform( const arma::mat& m );
     
     Shape transformed( const arma::mat& m ) const;
@@ -568,6 +573,118 @@ class Texture : public GfxObject
 		
 		bool	_initialized;	
 };
+
+
+	/// Depth stencil formats  
+    class DepthStencil
+    {
+    public:
+        
+        enum FORMAT
+        {
+            FORMAT_UNKNOWN = -1,
+            D16 = 0		,
+            D24,
+            D24_S8,
+            D32,
+            NUM_FORMATS
+        };
+    };
+    
+    
+#define MAX_COLOR_ATTACHMENTS	4 // THIS CAN BE DINAMICALLY CREATED BASED ON HW.
+    
+    
+#define DEFAULT_COLOR_FMT	Texture::A8R8G8B8
+#define DEFAULT_DEPTH_FMT	DepthStencil::D24_S8
+
+
+class RenderTarget : public GfxObject
+{
+public:
+    RenderTarget();
+    RenderTarget( int w, int h,
+                 int pixelFormat = DEFAULT_COLOR_FMT,
+                 bool depth = true,
+                 int depthFormat = DEFAULT_DEPTH_FMT,
+                 int numSamples = 0 );
+    
+    void zero();
+    
+    ~RenderTarget();
+    
+    /// Release render target
+    void	release();
+    
+    /// Create a 2D render target
+    /// \param w Width.
+    /// \param h Height.
+    /// \param pixelFormat Format for the render target texture.
+    /// \param depth If true also a depth buffer is created for the render target.
+    /// \param depthFormat Optional format for the depth buffer if present.
+    bool createMrt( int w, int h,
+                     std::vector<int> formats,
+                     bool hasDepthStencil = true,
+                     int depthStencilFormat = DEFAULT_DEPTH_FMT,
+                     int numSamples = 0 );
+    
+    bool create2d( int w, int h,
+                     int pixelFormat = DEFAULT_COLOR_FMT,
+                     bool depth = true,
+                     int depthFormat = DEFAULT_DEPTH_FMT,
+                     int numSamples = 0 )
+    {
+        std::vector<int> fmts;
+        fmts.push_back(pixelFormat);
+        return createMrt( w,h,fmts,depth,depthFormat,numSamples);
+    }
+    
+    
+    /// Returns true if render target has been initialized
+    bool	isValid() const;
+    
+    /// Resizes render target
+    bool	resize( int w, int h );
+    
+    /// Sets render target as current
+    void	bind( bool viewPort = true );
+    void	unbind( bool viewPort = true );
+    
+    Texture * getTexture( int ind = 0 );
+
+    void updateTexture( int ind = 0 );
+        
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+    
+
+protected:
+    GLuint createAndAttachRenderbuffer(GLenum internalFormat, GLenum attachmentPoint);
+    void createAndAttachTexture(int format, int attachmentPoint);
+    bool bindDepth();
+    
+    bool valid;
+    GLint savedFramebuffer;
+    
+    bool dirty[MAX_COLOR_ATTACHMENTS];
+    Texture * textureAttachments[MAX_COLOR_ATTACHMENTS];
+    GLuint colorBuffers[MAX_COLOR_ATTACHMENTS];
+    int numColorAttachments;
+    
+    std::vector<int> formats;
+    bool hasDepthStencil;
+    int depthStencilFormat;
+    int numSamples;
+
+    int depthStencilId;
+    GLuint fbo;
+    GLuint fboTextures;
+    
+    int width;
+    int height;
+
+};
+
 
 /// Wrapper around an OpenCV mat, 
 /// handles conversion to OpenGL textures
